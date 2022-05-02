44fb7d30f8f38b3a3c2593491cb2bca9ffbf21a7
alexcapoluongo <alexcapoluongo@icloud.com>
2022-05-02 15:48:11 +0200

    second trial bonus

--- js/script.js
+++ js/script.js
@@ -118,22 +118,11 @@ const items = [
 // Milestone 2
 // Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
 
-items.forEach((element) => {
-	let container = document.getElementById('container');
-	container.innerHTML += `<div class="item ${element.color}">
-							<i class ="${element.family} ${element.prefix}${element.name}"></i>
-							<p>${element.name}</p>
-							</div>`;
-});
-
-// BONUS
-// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-
 const animals = items.filter((element) => {
-	if (element.type === "animal") {
+	if(element.type === "animal") {
 		return true;
-	} 
-});
+	}
+})
 
 const vegetables = items.filter((element) => {
 	if(element.type === "vegetable") {
@@ -148,18 +137,39 @@ const users = items.filter((element) => {
 });
 
 
+items.forEach((element) => {
+	let container = document.getElementById('container');	
+	container.innerHTML += `<div id= ${element.type} class= "item">
+								<div class = "${element.color} ${element.type}">
+									<i class ="${element.family} ${element.prefix}${element.name}"></i>
+									<p>${element.name}</p>
+								</div>
+							</div>`
+});
+						
+// BONUS
+// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+
+
+// //creare un button con add eventlistener
+
 const filterBtn = document.getElementById('filter-button');
 //dare una variabile a select
+
 filterBtn.addEventListener('click', function(){
-	let userChoice = document.getElementById('filter').value;
-	if (userChoice === "animals") {
-		console.log(animals);
-	} else if (userChoice === "vegetables"){
-		console.log(vegetables);
-	} else if (userChoice === "users"){
-		console.log(users);
-	} else {
-		console.log(animals, vegetables, users);
-	}
+		let userChoice = document.getElementById('filter').value;	
+		if (userChoice === "animals") {
+			;
+		} else if (userChoice === "vegetables"){
+			const vegItem = document.getElementById('vegetable');
+			vegItem.classList.add('hidden');
+		} else if (userChoice === "users"){
+			let usersItem = document.getElementById('user');
+			usersItem.classList.add('hidden');
+		} else {
+			console.log(animals, vegetables, users);
+		}
 })
+
+
 
