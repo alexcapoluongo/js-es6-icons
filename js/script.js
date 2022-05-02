1f6ce398b9fc71fcf32337e1f73ce46c6e97cce8
alexcapoluongo <alexcapoluongo@icloud.com>
2022-05-02 16:01:18 +0200

    bonus added

--- js/script.js
+++ js/script.js
@@ -136,39 +136,41 @@ const users = items.filter((element) => {
 	}
 });
 
-
-items.forEach((element) => {
-	let container = document.getElementById('container');	
-	container.innerHTML += `<div id= ${element.type} class= "item">
-								<div class = "${element.color} ${element.type}">
-									<i class ="${element.family} ${element.prefix}${element.name}"></i>
-									<p>${element.name}</p>
-								</div>
-							</div>`
-});
-						
+createIcons(items);
+function createIcons(items) {
+	items.forEach((element) => {
+		let container = document.getElementById('container');	
+		container.innerHTML += `<div id= ${element.type} class= "item">
+									<div class = "${element.color} ${element.type}">
+										<i class ="${element.family} ${element.prefix}${element.name}"></i>
+										<p>${element.name}</p>
+									</div>
+								</div>`
+	});
+}					
 // BONUS
 // Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
 
 
 // //creare un button con add eventlistener
 
-const filterBtn = document.getElementById('filter-button');
+const selectItems = document.getElementById('filter');
 //dare una variabile a select
 
-filterBtn.addEventListener('click', function(){
-		let userChoice = document.getElementById('filter').value;	
-		if (userChoice === "animals") {
-			;
-		} else if (userChoice === "vegetables"){
-			const vegItem = document.getElementById('vegetable');
-			vegItem.classList.add('hidden');
-		} else if (userChoice === "users"){
-			let usersItem = document.getElementById('user');
-			usersItem.classList.add('hidden');
-		} else {
-			console.log(animals, vegetables, users);
-		}
+selectItems.addEventListener('input', function() {
+	let userChoice = document.getElementById('filter').value;
+	let container = document.getElementById('container');	
+	container.innerHTML = "";
+	
+	if (userChoice === "animals") {
+		createIcons(animals);
+	} else if (userChoice === "vegetables"){
+		createIcons(vegetables);
+	} else if (userChoice === "users"){
+		createIcons(users);
+	} else {
+		console.log(animals, vegetables, users);
+	}
 })
 
 
