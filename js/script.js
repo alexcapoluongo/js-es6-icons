ce68e80a2e52ab0f9281ce4c9ec28aa591ed2191
alexcapoluongo <alexcapoluongo@icloud.com>
2022-05-02 13:35:27 +0200

    milestone 1 added

--- js/script.js
+++ js/script.js
@@ -1,114 +1,128 @@
-[
-	{
-		name: 'cat',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'crow',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'dog',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'dove',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'dragon',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'horse',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'hippo',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'fish',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'carrot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
-	},
-	{
-		name: 'apple-alt',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
-	},
-	{
-		name: 'lemon',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
-	},
-	{
-		name: 'pepper-hot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
-	},
-	{
-		name: 'user-astronaut',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
-	},
-	{
-		name: 'user-graduate',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
-	},
-	{
-		name: 'user-ninja',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
-	},
-	{
-		name: 'user-secret',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
-	}
+const items = [
+				{
+					name: 'cat',
+					prefix: 'fa-',
+					type: 'animal',
+					family: 'fas',
+					color: 'orange'
+				},
+				{
+					name: 'crow',
+					prefix: 'fa-',
+					type: 'animal',
+					family: 'fas',
+					color: 'orange'
+				},
+				{
+					name: 'dog',
+					prefix: 'fa-',
+					type: 'animal',
+					family: 'fas',
+					color: 'orange'
+				},
+				{
+					name: 'dove',
+					prefix: 'fa-',
+					type: 'animal',
+					family: 'fas',
+					color: 'orange'
+				},
+				{
+					name: 'dragon',
+					prefix: 'fa-',
+					type: 'animal',
+					family: 'fas',
+					color: 'orange'
+				},
+				{
+					name: 'horse',
+					prefix: 'fa-',
+					type: 'animal',
+					family: 'fas',
+					color: 'orange'
+				},
+				{
+					name: 'hippo',
+					prefix: 'fa-',
+					type: 'animal',
+					family: 'fas',
+					color: 'orange'
+				},
+				{
+					name: 'fish',
+					prefix: 'fa-',
+					type: 'animal',
+					family: 'fas',
+					color: 'orange'
+				},
+				{
+					name: 'carrot',
+					prefix: 'fa-',
+					type: 'vegetable',
+					family: 'fas',
+					color: 'green'
+				},
+				{
+					name: 'apple-alt',
+					prefix: 'fa-',
+					type: 'vegetable',
+					family: 'fas',
+					color: 'green'
+				},
+				{
+					name: 'lemon',
+					prefix: 'fa-',
+					type: 'vegetable',
+					family: 'fas',
+					color: 'green'
+				},
+				{
+					name: 'pepper-hot',
+					prefix: 'fa-',
+					type: 'vegetable',
+					family: 'fas',
+					color: 'green'
+				},
+				{
+					name: 'user-astronaut',
+					prefix: 'fa-',
+					type: 'user',
+					family: 'fas',
+					color: 'blue'
+				},
+				{
+					name: 'user-graduate',
+					prefix: 'fa-',
+					type: 'user',
+					family: 'fas',
+					color: 'blue'
+				},
+				{
+					name: 'user-ninja',
+					prefix: 'fa-',
+					type: 'user',
+					family: 'fas',
+					color: 'blue'
+				},
+				{
+					name: 'user-secret',
+					prefix: 'fa-',
+					type: 'user',
+					family: 'fas',
+					color: 'blue'
+				}
 ];
+
+// Milestone 1
+// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+
+items.forEach((element) => {
+	let container = document.getElementById('container');
+	container.innerHTML += `<div class="item">
+							<i class ="${element.family} ${element.prefix}${element.name}"></i>
+							<p>${element.name}</p>
+							</div>`;
+});
+
+// Milestone 2
+// Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
